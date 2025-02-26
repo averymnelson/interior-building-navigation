f2594c3c8c5e53c2b26a70c6061e0bafe11ce25e
Christian Morales <112047052+ChrisMorale@users.noreply.github.com>
2025-02-26 16:36:12 -0600

    Add files via upload
    Swapped node ids to str, added print functions

--- navigation_system/models/node.py
+++ navigation_system/models/node.py
@@ -3,24 +3,27 @@ from typing import List, Tuple
 
 @dataclass
 class Node:
-    id: int
+    id: str
     type_name: str
     layer: int
     x: float
     y: float
-    connections: List[Tuple[int, float]] = field(default_factory=list)
+    connections: List[Tuple[str, float]] = field(default_factory=list)
 
-    def add_connection(self, node_id: int, weight: float):
+    def add_connection(self, node_id: str, weight: float):
         self.connections.append((node_id, weight))
+    def print(self):
+        print(f"ID: {self.id}, Type: {self.type_name}, Layer: {self.layer}, X: {self.x}, Y: {self.y}, Connections: {self.connections}")
+
 
 class NavigationGraph:
     def __init__(self):
         self.nodes = {}
 
-    def add_node(self, id: int, type_name: str, layer: int, x: float, y: float) -> None:
+    def add_node(self, id: str, type_name: str, layer: int, x: float, y: float) -> None:
         self.nodes[id] = Node(id, type_name, layer, x, y)
 
-    def add_edge(self, node1_id: int, node2_id: int) -> None:
+    def add_edge(self, node1_id: str, node2_id: str) -> None:
         node1 = self.nodes[node1_id]
         node2 = self.nodes[node2_id]
         weight = self._calculate_distance(node1, node2)
@@ -28,6 +31,10 @@ class NavigationGraph:
         node1.add_connection(node2_id, weight)
         node2.add_connection(node1_id, weight)
 
+    def print_Nodes(self) -> None:
+        for node in self.nodes.values():
+            node.print()
+
     def _calculate_distance(self, node1: Node, node2: Node) -> float:
-        return ((node1.x - node2.x)**2 + (node1.y - node2.y)**2)**0.5
+        return ((float(node1.x) - float(node2.x))**2 + (float(node1.y) - float(node2.y))**2)**0.5
     
